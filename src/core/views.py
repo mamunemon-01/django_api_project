19612408c367598d9dcddbe6b77cb33c0eaa1a5d
Mamun Al Imran <sciemontist@gmail.com>
2026-01-05 14:13:00 +0600

    A string method has been introdueced to the Post model and Serializers have been implemented for GET and POST requests for the Post model. The Post model has also been registered for the site for django admin panel entries.

--- src/core/views.py
+++ src/core/views.py
@@ -4,17 +4,29 @@ from django.shortcuts import render
 # third-party imports
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import PostSerializer
+from .models import Post
 
 # Create your views here.
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            "country": "Venezuela",
-            "message": "Donald Trump has President Maduro kidnapped!"
-        }
-        
-        return Response(data)
+        # data = {
+        #     "country": "Venezuela",
+        #     "message": "Donald Trump has President Maduro kidnapped!"
+        # }
+        qset = Post.objects.all()
+        # serializer = PostSerializer(qset, many=True)
+        data = qset.first()
+        serializer = PostSerializer(data)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 # def test_view(request):
 #     data = {
