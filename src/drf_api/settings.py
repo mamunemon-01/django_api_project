a186fb973c290c2836cba907901be6a2781a52c1
Mamun Al Imran <sciemontist@gmail.com>
2026-01-06 14:28:05 +0600

    dj-rest-auth package has been integrated for creating token upon login

--- src/drf_api/settings.py
+++ src/drf_api/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'core'
 ]
 
 MIDDLEWARE = [
--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -21,6 +21,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('admin/', admin.site.urls),
     path('', TestView.as_view()),
     path('api/token/', obtain_auth_token, name='obtain-token')
