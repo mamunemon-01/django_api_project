ad47d516079f541d215a1ae9cefa5eb0b764082e
Mamun Al Imran <sciemontist@gmail.com>
2026-01-08 12:57:26 +0600

    The design of th e post elements has been edited.

--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -3,9 +3,9 @@
     <h3 class="mb-3">News Feed</h3>
     <div class="PostsContainer">
       <div v-for="post in posts" :key="post.id" class="card mb-3">
-        <div class="card-body d-flex justify-content-left flex-column">
-          <h5 class="card-title text-left">{{ post.title }}</h5>
-          <p class="card-text">{{ post.description }}</p>
+        <div class="card-body d-flex justify-content-start flex-column">
+          <h5 class="card-title text-start p-2">{{ post.title }}</h5>
+          <p class="card-text text-start p-3">{{ post.description }}</p>
         </div>
       </div>
     </div>
@@ -43,4 +43,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .card-title {
+    background-color: #d0d0d0;
+    /* color: ; */
+    border-radius: 4px;
+  }
 </style>
