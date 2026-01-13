5a8c67407f74dec73c52953e6b24a642ce90a87f
Mamun Al Imran <sciemontist@gmail.com>
2026-01-13 19:31:15 +0600

    Username alignment, app title and navigation menu names have been improved.

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <router-link to="/posts" class="navbar-brand fw-bold">HeadBook</router-link>
+            <router-link to="/posts" class="navbar-brand"><h1 class="fw-bold">HeadBook</h1></router-link>
             <div class="navbar-nav">
-                <router-link to="/posts" class="nav-link text-light">Posts</router-link>
-                <router-link to="/products" class="nav-link text-light ms-3">Products</router-link>
-                <h3 id="user-name" class="ms-4 text-light fw-bold border rounded-5 p-1">@{{ username }}</h3>
+                <router-link to="/posts" class="nav-link text-light fw-bold">Posts</router-link>
+                <router-link to="/products" class="nav-link text-light fw-bold ms-3">Products</router-link>
+                <h3 id="user-name" class="mx-4 text-light fw-bold border rounded-5 p-1">@{{ username }}</h3>
                 <div class="ms-auto pe-2 py-1">
                     <button class="btn btn-light fw-bold" @click="$emit('logout')">Log Out</button>
                 </div>
